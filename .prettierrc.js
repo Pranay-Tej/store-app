1738f4472f4d9f9fbd61947ff8b01fa8fdb72b55
Pranay-Tej <34662441+Pranay-Tej@users.noreply.github.com>
2021-10-21 11:36:25 +0530

    config: prettier, install packages

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'lf',
+  trailingComma: 'none',
+  proseWrap: 'always',
+  arrowParens: 'avoid'
+};
