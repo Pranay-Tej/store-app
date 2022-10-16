6929626f462df170daacb624d550d8ae2ce9c279
Pranay-Tej <s.pranayteja@gmail.com>
2022-10-16 22:26:32 +0530

    config: cypress

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    experimentalSessionAndOrigin: true,
+    baseUrl: process.env.CYPRESS_baseUrl || 'http://localhost:8888'
+  },
+  experimentalStudio: true,
+  defaultCommandTimeout: 5000
+});
