70882e3fc0f6a708de53172be63792aac67ba0c2
Pranay-Tej <s.pranayteja@gmail.com>
2024-06-27 21:49:13 +0530

    add view-transition

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
 	import '../app.css';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <slot />
